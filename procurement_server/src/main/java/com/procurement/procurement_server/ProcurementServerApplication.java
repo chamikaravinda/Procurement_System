48e500f82eda03b09dcee206da281fce60aebd43
Chamika Ravinda <chamikaravinda@gmail.com>
2019-09-19 07:50:48 +0530

    adding files

---
+++ procurement_server/src/main/java/com/procurement/procurement_server/ProcurementServerApplication.java
@@ -0,0 +1,13 @@
+package com.procurement.procurement_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcurementServerApplication.class, args);
+	}
+
+}
