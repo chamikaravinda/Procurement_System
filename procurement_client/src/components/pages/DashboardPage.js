4fcdc0c17ea28fd9225db2e2b3ff3369cb8895ac
Chamika Ravinda <chamikaravinda@gmail.com>
2019-09-20 07:36:43 +0530

    adding the thema for the client

---
+++ procurement_client/src/components/pages/DashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const DashboardPage =  () => {
+  return (
+    <React.Fragment>
+      <h6>Wlcome</h6>
+    </React.Fragment>
+  )
+}
+
+export default DashboardPage;
