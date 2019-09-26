3f940077c2ac04e6fd2a6289294e3289d200ce0c
deshan <deshanshehantha@gmail.com>
2019-09-26 11:59:16 +0530

    user login started with the db model and class modeling with user levels

---
+++ procurement_server/src/main/java/com/procurement/procurement_server/model/user_level/UserAdaptor.java
@@ -0,0 +1,60 @@
+package com.procurement.procurement_server.model.user_level;
+
+public class UserAdaptor {
+
+    private String firstName;
+    private String lastName;
+    private String type;
+    private String email;
+    private String password;
+    private Object userType;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Object getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Object userType) {
+        this.userType = userType;
+    }
+
+}
